13c8ec3243f4fddaf172b3593e3f8a92bc08f4a0
Sam Witteveen <sam@samwitteveen.com>
2017-08-14 16:05:41 +0800

    Initial Commit

---
+++ SatayNotSatay/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SatayNotSatay
+//
+//  Created by Sam Witteveen on 14/8/17.
+//  Copyright © 2017 Sam Witteveen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
